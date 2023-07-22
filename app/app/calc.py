349fb2dfb2b12648a069eafdee469ea61094a171
Kushal Dotel <kushal.dotel@study.lbef.edu.np>
2023-07-22 19:53:03 +0545

    making the databases migrations

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,12 +1,16 @@
 """
 Calculator Functions
-
 """
+
 
 def add(x, y):
     """Add Function"""
     return x + y
 
+
 def subtract(x, y):
     """Subtract Function"""
     return x - y
+
+
+# Add a blank line at the end of the file
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,13 +6,15 @@ from django.test import SimpleTestCase
 
 from . import calc
 
+
 class CalcTest(SimpleTestCase):
     '''Test module for calc.py'''
+
     def test_add_numbers(self):
         '''Test add function'''
         res = calc.add(3, 8)
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         '''Test subtract function'''
         res = calc.subtract(3, 8)
